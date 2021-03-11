d9de2a22ef629c062d10578f39496b4451808b9f
Sidney Miranda <sidneysmiranda@hotmail.com>
2021-03-11 00:45:41 -0300

    Added feature GET /api/v1/countries/{id}

--- src/main/java/com/github/sidneymiranda/citiesapi/resources/CountryResource.java
+++ src/main/java/com/github/sidneymiranda/citiesapi/resources/CountryResource.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,10 @@ public class CountryResource {
     @ResponseStatus(HttpStatus.OK)
     public Page<Country> countryList(Pageable pageable){
         return service.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getCountryById(@PathVariable Long id) {
+        return service.getOne(id);
     }
 }
--- src/main/java/com/github/sidneymiranda/citiesapi/services/CountryService.java
+++ src/main/java/com/github/sidneymiranda/citiesapi/services/CountryService.java
@@ -7,7 +7,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor(onConstructor_ = {@Autowired})
@@ -17,5 +20,13 @@ public class CountryService {
 
     public Page<Country> findAll(Pageable pageable){
         return repository.findAll(pageable);
+    }
+
+    public ResponseEntity<Object> getOne(Long id){
+        Optional<Country> optional = repository.findById(id);
+        if(optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        }
+        return  ResponseEntity.notFound().build();
     }
 }
