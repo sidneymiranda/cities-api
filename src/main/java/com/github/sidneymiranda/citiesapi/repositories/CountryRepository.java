a2622d0848bdb143fd2761a0456fdd99d0811ad4
Sidney Miranda <sidneysmiranda@hotmail.com>
2021-03-11 02:11:45 -0300

    Added features: GET /api/v1/states and GET /api/v1/states/{id}

--- src/main/java/com/github/sidneymiranda/citiesapi/repositories/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.sidneymiranda.citiesapi.repositories;
-
-import com.github.sidneymiranda.citiesapi.entities.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
